b63c44c9e785e34d7abfa65988acb44daf6b6715
appsaf2k <appsaf2k@gmail.com>
2022-09-08 10:04:09 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by @andreev2k on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
